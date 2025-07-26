5a32402ca7b967e564cf51cc641d9476cb0a2c99
josepablo.lopez <jose.p.lopez.n@gmail.com>
2025-07-26 08:04:19 -0600

    added the methods used to populate a nonogram

--- index.js
+++ index.js
@@ -4,3 +4,43 @@ import express from "express";
 const app = express();
 const port = 3000;
 
+var rangex = [];
+var rangey = [];
+
+app.get("/", (req,res)=>{
+    res.render("index.ejs");
+});
+
+app.listen(port,()=>{
+    console.log(`Listening on port ${port}`);
+});
+
+function populate(gridx,gridy){
+    for(var i=0; i <gridx; i++){
+        rangex.push(i);
+    }
+
+    for( var i=0; i<gridy ;i++){
+        rangey.push(i);
+    }
+}
+
+function generate(gridx, gridy){
+
+    const numberOfTiles = Math.floor(Math.random()*(gridx*gridy)) + 1;
+    var coordinates = [];
+
+    populate(gridx, gridy);
+    
+    for(var i=0; i <= numberOfTiles ; i++){
+        var randomIndexX = Math.floor(Math.random() * rangex.length);
+        var randomIndexY = Math.floor(Math.random() * rangey.length);
+
+        coordinates.push([rangex[randomIndexX], rangey[randomIndexY]]);
+    }
+
+    console.log(coordinates);
+
+}
+
+generate(10,2);
